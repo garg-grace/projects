5343532da650601780fa6d9969d558506d4d7ca6
Grace garg <graceg.tt.19@nitj.ac.in>
2021-05-14 19:08:27 +0530

    attendance system

---
+++ smart_attendance-system/new.py
@@ -0,0 +1,33 @@
+from pyzbar.pyzbar import decode
+from PIL import Image
+import cv2 ,time
+import csv
+
+video = cv2.VideoCapture(0)
+
+students=[]
+
+with open("database.csv.csv","r") as file:
+    reader=csv.reader(file)
+    for row in reader:
+        students.append((row[1]))
+    print(students)    
+while True:
+    check,frame=video.read()
+    d=decode(frame)
+    try:
+        for obj in d:
+            name=d[0].data.decode()
+            if name in students:
+                students.remove(name)
+                print("deleted......")
+    except:
+        print("error")
+
+    cv2.imshow("Attendence", frame)
+    key=cv2.waitKey(1)
+    if key==ord("q"):
+        print (students)
+        break
+video.release()
+cv2.destroyAllwindows()
